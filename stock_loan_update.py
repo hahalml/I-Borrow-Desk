222dc2ddc05269dcd17a1864d8ecfd40df9ef94a
cjmochrie <cameron.mochrie@gmail.com>
2015-06-26 14:33:11 -0400

    Better styling, clickable rows for watchlist

--- stock_loan_update.py
+++ stock_loan_update.py
@@ -1,10 +1,6 @@
 
 # Just a script for cronjob to run every during trading hours to update the database
-import time
 from BorrowDatabase import BorrowDatabase
-
-# Sleep 5 seconds (enough time to grab the FTP files at 3s past the hour)
-time.sleep(10)
 
 stockLoan = BorrowDatabase(database_name='stock_loan', filename='usa', create_new=False)
 stockLoan.update()
