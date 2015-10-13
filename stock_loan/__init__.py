97beef63f55db478c3991a347415ea343f9bc4e0
cjmochrie <cameron.mochrie@gmail.com>
2015-10-13 15:41:32 -0400

    Enable email of errors. Configure templates to default link to 'historical' views

--- stock_loan/__init__.py
+++ stock_loan/__init__.py
@@ -1,7 +1,10 @@
 APPLICATION_NAME = "Stock Loan App"
 
-import memcache
+import logging
+from logging.handlers import SMTPHandler, QueueHandler, QueueListener
+import queue
 
+import memcache
 from flask import Flask
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -11,11 +14,11 @@ from flask_admin import Admin
 from .borrow import Borrow
 
 # Create a Flask instance
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
+app.config.from_pyfile('config.py')
 sslify = SSLify(app, skips=['main_page', 'about', 'faq'])
 admin = Admin(app)
-
 
 db = SQLAlchemy(app)
 
@@ -35,3 +38,24 @@ admin.add_view(DbView(User, db.session))
 
 #Build the database
 db.create_all()
+
+# logging
+if not app.debug:
+    que = queue.Queue(-1) # no limit on size
+    # Create a QueueHandler to receive logging
+    queue_handler = QueueHandler(que)
+    queue_handler.setLevel(logging.ERROR)
+
+    # Create the actual mail handler
+    credentials = app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD']
+    mail_handler = SMTPHandler(('smtp.gmail.com', '587'), app.config['APP_ADDRESS'], [app.config['ADMIN_ADDRESS']],
+                               'stock_loan exception', credentials=credentials, secure=())
+
+    # Create a listener handler to deque things from the QueueHandler and send to the mail handler
+    listener = QueueListener(que, mail_handler)
+    listener.start()
+
+    # Add the queue handler to the app
+    app.logger.addHandler(queue_handler)
+
+
